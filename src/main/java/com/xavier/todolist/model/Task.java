ef90a5961ac6846ebb4c9f378bfdbc509321dc3a
Anthony xavier <anthonyltx12@gmail.com>
2025-08-28 10:27:11 -0300

    Implementação da classe task

--- src/main/java/com/xavier/todolist/model/Task.java
+++ src/main/java/com/xavier/todolist/model/Task.java
@@ -1,4 +1,88 @@
 package com.xavier.todolist.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@Entity
+@Table(name = "tasks")
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String descricao;
+    private String status;
+    private String prioridade;
+    private LocalDate dataCriacao;
+    private LocalDate dataConclusao;
+
+    public Task(){}
+
+    public Task(String titulo, String descricao, String status, String prioridade, LocalDate dataCriacao, LocalDate dataConclusao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.status = status;
+        this.prioridade = prioridade;
+        this.dataCriacao = dataCriacao;
+        this.dataConclusao = dataConclusao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(String prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public LocalDate getDataConclusao() {
+        return dataConclusao;
+    }
+
+    public void setDataConclusao(LocalDate dataConclusao) {
+        this.dataConclusao = dataConclusao;
+    }
 }
